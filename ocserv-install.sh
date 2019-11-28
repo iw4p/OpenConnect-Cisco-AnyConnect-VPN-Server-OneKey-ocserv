7843a8147237ec254c11d2cdad4b15a9932cd7b3
iw4p <nimakbarzade@yahoo.com>
2019-11-28 11:45:55 +0330

    remove blink

--- ocserv-install.sh
+++ ocserv-install.sh
@@ -31,7 +31,7 @@ echo '
 ip=$(hostname -I|cut -f1 -d ' ')
 echo "your Server IP address is:$ip"
 
-echo -e "\e[5m\e[92mInstalling gnutls-bin"
+echo -e "\e[32mInstalling gnutls-bin"
 
 apt install gnutls-bin
 mkdir certificates
@@ -64,7 +64,7 @@ EOF
 certtool --generate-privkey --outfile server-key.pem
 certtool --generate-certificate --load-privkey server-key.pem --load-ca-certificate ca-cert.pem --load-ca-privkey ca-key.pem --template server.tmpl --outfile server-cert.pem
 
-echo -e "\e[5m\e[92mInstall ocserv"
+echo -e "\e[32mInstall ocserv"
 apt install ocserv
 cp /etc/ocserv/ocserv.conf ~/certificates/
 pass="passwd=/etc/ocserv/ocpasswd"
@@ -83,9 +83,9 @@ iptables -t nat -A POSTROUTING -j MASQUERADE
 sed -i -e 's@#net.ipv4.ip_forward@net.ipv4.ip_forward=1@g' /etc/ocserv/ocserv.conf
 
 sysctl -p /etc/sysctl.conf
-echo -e "\e[92mStopping ocserv service"
+echo -e "\e[32mStopping ocserv service"
 service ocserv stop
-echo -e "\e[92mStarting ocserv service"
+echo -e "\e[32mStarting ocserv service"
 service ocserv start
 
 echo "\e[32mOpenConnect Server Configured Succesfully."
