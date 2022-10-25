fdcf6ca1c7aac1f461006d34b3d129a1550a5bdd
shahriarb <shahriar.b@gmail.com>
2022-10-25 21:35:59 +0100

    test

--- build-server.sh
+++ build-server.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 if [ "$(ls -A cert)" ]; then
-  DOCKER_FILE="Dockerfile"
-else
   DOCKER_FILE="Dockerfile.cert"
+else
+  DOCKER_FILE="Dockerfile"
 fi
+
+echo $DOCKER_FILE
 
 docker build -t ocserv -f "$DOCKER_FILE" .
